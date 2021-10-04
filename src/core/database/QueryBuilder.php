fcdab9224f628511cd7f7678ccbf1c243a7bf97d
MarkusAleksander <markholden1989@googlemail.com>
2021-10-04 13:55:56 +0100

    Initial Query Builder added

--- src/core/database/QueryBuilder.php
+++ src/core/database/QueryBuilder.php
@@ -13,5 +13,42 @@ class QueryBuilder
     protected $pdo;
 
     // * Ensure an active connection is passed on creating the QueryBuilder
+    public function __construct($pdo)
+    {
+        // * TODO - check $pdo has been passed
+        $this->pdo = $pdo;
+    }
 
+    // * Select All
+    public function selectAll($table)
+    {
+        // * Prepare the statement
+        $statement = $this->pdo->prepare("select * from {$table}");
+
+        // * Execute
+        $statement->execute();
+
+        // * Return results
+        return $statement->fetchAll(\PDO::FETCH_CLASS);
+    }
+
+    // * Insert result
+    public function insert($table, $params)
+    {
+        // * extract $param keys into a placeholder statement
+        $sql = sprintf(
+            'insert into %s (%s) values ($s)',
+            $table,
+            implode(',', array_keys($params)),
+            ':' . implode(', :', array_keys($params))
+        );
+
+        try {
+            $statement = $this->pdo->prepare($sql);
+
+            $statement->execute($params);
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+    }
 }
